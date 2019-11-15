5a7b06f65bf3c07ea45cf9ff00d46e6696eaf79b
Thản Nguyễn <thannguyen199789@gmail.com>
2019-11-15 09:25:01 +0700

    update delete

---
+++ client/src/app/Idea/deleteIdea.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core'
+import { AuthenticationService,IdeaDetails} from '../authentication.service'
+import { Router } from '@angular/router'
+import { db } from '../../../../database/db';
+@Component({
+    templateUrl: './deleteIdea.component.html'
+})
+export class deleteIdeaComponent {
+    credentials: IdeaDetails = {
+        id: 0,
+        name: '',
+        description: ''
+    }
+    constructor(private auth: AuthenticationService, private router: Router) { }
+    deleteIdea() {
+        this.auth.deleteIdea(this.credentials.id).subscribe(
+            idea => {
+                alert('idea đã được xóa')
+                this.router.navigateByUrl('/listIdea')
+            },
+            err => {
+                console.error(err)    
+            }
+        )
+    }
+}
